e235739a0dc07ee27579ef47217687187e932d2e
rp4 <rp4@users.noreply.github.com>
2025-08-28 16:00:48 -0500

    Fix overtime and utilization sorting when clicking metrics - Added setTimeout to ensure sorting triggers after navigation completes - Triggers now fire after component mount to ensure proper state updates - Fixed timing issue where navigation was happening before trigger was set The sorting now works correctly: - Clicking Overtime Hours → Hours tab with employees sorted by overtime (highest first) - Clicking Resource Utilization → Hours tab with employees sorted by utilization (lowest first)

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -248,7 +248,7 @@ module.exports = require("util");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, ["vendor","common-_rsc_node_modules_next_dist_build_webpack_loaders_next-flight-client-entry-loader_js_m-dfccd5"], () => (__webpack_exec__("(rsc)/./node_modules/next/dist/build/webpack/loaders/next-app-loader/index.js?name=app%2Fpage&page=%2Fpage&appPaths=%2Fpage&pagePath=private-next-app-dir%2Fpage.tsx&appDir=%2Fhome%2Fp472%2FScheduler%2Fapp&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&rootDir=%2Fhome%2Fp472%2FScheduler&isDev=true&tsconfigPath=tsconfig.json&basePath=&assetPrefix=&nextConfigOutput=export&preferredRegion=&middlewareConfig=e30%3D&isGlobalNotFoundEnabled=!")));
+var __webpack_exports__ = __webpack_require__.X(0, ["vendor","common-_rsc_node_modules_next_dist_build_webpack_loaders_next-flight-client-entry-loader_js_m-2b1972","common-_ssr_lib_utils_ts-_ssr_store_useScheduleStore_ts"], () => (__webpack_exec__("(rsc)/./node_modules/next/dist/build/webpack/loaders/next-app-loader/index.js?name=app%2Fpage&page=%2Fpage&appPaths=%2Fpage&pagePath=private-next-app-dir%2Fpage.tsx&appDir=%2Fhome%2Fp472%2FScheduler%2Fapp&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&rootDir=%2Fhome%2Fp472%2FScheduler&isDev=true&tsconfigPath=tsconfig.json&basePath=&assetPrefix=&nextConfigOutput=export&preferredRegion=&middlewareConfig=e30%3D&isGlobalNotFoundEnabled=!")));
 module.exports = __webpack_exports__;
 
 })();
